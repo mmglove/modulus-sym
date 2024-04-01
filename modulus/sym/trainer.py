88daca50c26dfff54957f0376821d3aa662cd57c
HydrogenSulfate <490868991@qq.com>
2024-04-01 06:48:58 +0000

    enable validator, inferencer, monitor in trainer.py

--- modulus/sym/trainer.py
+++ modulus/sym/trainer.py
@@ -652,23 +652,23 @@ class Trainer(AdamMixin, AdaHessianMixin, BFGSMixin):
                 #     barrier_flag = True
                 #     self._record_constraints()
 
-                # if (step % self.cfg.training.rec_validation_freq == 0) and (
-                #     self.has_validators
-                # ):
-                #     barrier_flag = True
-                #     self._record_validators(step)
+                if (step % self.cfg.training.rec_validation_freq == 0) and (
+                    self.has_validators
+                ):
+                    barrier_flag = True
+                    self._record_validators(step)
 
-                # if (step % self.cfg.training.rec_inference_freq == 0) and (
-                #     self.has_inferencers
-                # ):
-                #     barrier_flag = True
-                #     self._record_inferencers(step)
+                if (step % self.cfg.training.rec_inference_freq == 0) and (
+                    self.has_inferencers
+                ):
+                    barrier_flag = True
+                    self._record_inferencers(step)
 
-                # if (step % self.cfg.training.rec_monitor_freq == 0) and (
-                #     self.has_monitors
-                # ):
-                #     barrier_flag = True
-                #     self._record_monitors(step)
+                if (step % self.cfg.training.rec_monitor_freq == 0) and (
+                    self.has_monitors
+                ):
+                    barrier_flag = True
+                    self._record_monitors(step)
 
                 # save checkpoint
                 if step % self.save_network_freq == 0:
