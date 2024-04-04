bed361968a04a056e0740e9cc7dfe34a3583b351
HydrogenSulfate <490868991@qq.com>
2024-04-04 06:36:30 +0000

    add init weight loading code(annotated by default)

--- modulus/sym/trainer.py
+++ modulus/sym/trainer.py
@@ -504,13 +504,18 @@ class Trainer(AdamMixin, AdaHessianMixin, BFGSMixin):
             )
 
         # load network
+        # self.initial_step = 0
+        # for model in self.saveable_models:
+        #     model.set_state_dict(
+        #         paddle.load(f"/workspace/hesensen/modulus_pd_th_bkd_compare/modulus-sym/examples/chip_2d/outputs/cuda_graphs=false,graph.func_arch=false,graph.func_arch_allow_partial_hessian=false,jit=false,jit_use_nvfuser=false,training.max_steps=50/chip_2d/init_ckpt/{model.checkpoint_filename}")
+        #     )
         debug_flag = bool(int(os.getenv("debug", False)))
         if debug_flag:
             self.log.info("✨ ✨ Skip load network as debug=1 in os.getenv")
             self.initial_step = 0
             for model in self.saveable_models:
                 model.set_state_dict(
-                    paddle.load(f"init_ckpt/{model.checkpoint_filename}")
+                    paddle.load(f"/workspace/hesensen/modulus_pd_th_bkd_compare/modulus-sym/examples/chip_2d/outputs/cuda_graphs=false,graph.func_arch=false,graph.func_arch_allow_partial_hessian=false,jit=false,jit_use_nvfuser=false,training.max_steps=50/chip_2d/init_ckpt/{model.checkpoint_filename}")
                 )
         else:
             self.initial_step = self.load_network()
