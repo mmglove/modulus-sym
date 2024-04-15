01d6c3f447c9846942e211918bdd25c2b01d43a5
HydrogenSulfate <490868991@qq.com>
2024-04-15 06:50:08 +0000

    add N1C1 files

---
+++ test_tipc/dynamicTostatic/ldc-ldc_2d_domain_decomposition/N1C1/ldc-ldc_2d_domain_decomposition_bs1_fp32_DP.sh
@@ -0,0 +1,16 @@
+model_item=ldc-ldc_2d_domain_decomposition_bs1_fp32_DP
+bs_item=1
+fp_item=fp32
+run_mode=DP
+device_num=N1C1
+pip config set global.index-url https://mirrors.ustc.edu.cn/pypi/web/simple
+# copy files
+\cp test_tipc/dynamicTostatic/ldc-ldc_2d_domain_decomposition/benchmark_common/prepare.sh ./
+\cp test_tipc/dynamicTostatic/ldc-ldc_2d_domain_decomposition/benchmark_common/run_benchmark.sh ./
+\cp test_tipc/dynamicTostatic/ldc-ldc_2d_domain_decomposition/N1C1/ldc-ldc_2d_domain_decomposition_bs1_fp32_DP.sh ./
+\cp test_tipc/dynamicTostatic/ldc-ldc_2d_domain_decomposition/benchmark_common/analysis_log.py ./
+# prepare
+bash prepare.sh
+# run
+to_static=True FLAGS_enable_pir_in_executor=true FLAGS_enable_pir_api=True FLAGS_cinn_bucket_compile=True FLAGS_group_schedule_tiling_first=1 FLAGS_cinn_new_group_scheduler=1 FLAGS_nvrtc_compile_to_cubin=True bash run_benchmark.sh ${model_item} ${bs_item} ${fp_item} ${run_mode} ${device_num} 2>&1;
+sleep 10;
---
+++ test_tipc/dynamicTostatic/ode_spring_mass-spring_mass_solver/N1C1/ode_spring_mass-spring_mass_solver_bs1_fp32_DP.sh
@@ -0,0 +1,16 @@
+model_item=ode_spring_mass-spring_mass_solver_bs1_fp32_DP
+bs_item=1
+fp_item=fp32
+run_mode=DP
+device_num=N1C1
+pip config set global.index-url https://mirrors.ustc.edu.cn/pypi/web/simple
+# copy files
+\cp test_tipc/dynamicTostatic/ode_spring_mass-spring_mass_solver/benchmark_common/prepare.sh ./
+\cp test_tipc/dynamicTostatic/ode_spring_mass-spring_mass_solver/benchmark_common/run_benchmark.sh ./
+\cp test_tipc/dynamicTostatic/ode_spring_mass-spring_mass_solver/N1C1/ode_spring_mass-spring_mass_solver_bs1_fp32_DP.sh ./
+\cp test_tipc/dynamicTostatic/ode_spring_mass-spring_mass_solver/benchmark_common/analysis_log.py ./
+# prepare
+bash prepare.sh
+# run
+to_static=True FLAGS_enable_pir_in_executor=true FLAGS_enable_pir_api=True FLAGS_cinn_bucket_compile=True FLAGS_group_schedule_tiling_first=1 FLAGS_cinn_new_group_scheduler=1 FLAGS_nvrtc_compile_to_cubin=True bash run_benchmark.sh ${model_item} ${bs_item} ${fp_item} ${run_mode} ${device_num} 2>&1;
+sleep 10;
