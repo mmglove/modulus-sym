bb66350ad72b5e09e90254734a556532fbe83c86
HydrogenSulfate <490868991@qq.com>
2024-04-17 06:49:33 +0000

    add loss for annular_ring.py

--- examples/annular_ring/annular_ring/annular_ring.py
+++ examples/annular_ring/annular_ring/annular_ring.py
@@ -86,6 +86,7 @@ def run(cfg: ModulusConfig) -> None:
         outvar={"u": inlet_sympy, "v": 0},
         batch_size=cfg.batch_size.inlet,
         criteria=Eq(x, channel_length[0]),
+        loss=modulus.sym.loss.PointwiseLossNorm(name="inlet"),
     )
     domain.add_constraint(inlet, "inlet")
 
@@ -96,6 +97,7 @@ def run(cfg: ModulusConfig) -> None:
         outvar={"p": 0},
         batch_size=cfg.batch_size.outlet,
         criteria=Eq(x, channel_length[1]),
+        loss=modulus.sym.loss.PointwiseLossNorm(name="outlet"),
     )
     domain.add_constraint(outlet, "outlet")
 
@@ -106,6 +108,7 @@ def run(cfg: ModulusConfig) -> None:
         outvar={"u": 0, "v": 0},
         batch_size=cfg.batch_size.no_slip,
         criteria=And((x > channel_length[0]), (x < channel_length[1])),
+        loss=modulus.sym.loss.PointwiseLossNorm(name="no_slip"),
     )
     domain.add_constraint(no_slip, "no_slip")
 
@@ -120,6 +123,7 @@ def run(cfg: ModulusConfig) -> None:
             "momentum_x": Symbol("sdf"),
             "momentum_y": Symbol("sdf"),
         },
+        loss=modulus.sym.loss.PointwiseLossNorm(name="interior"),
     )
     domain.add_constraint(interior, "interior")
 
@@ -132,6 +136,7 @@ def run(cfg: ModulusConfig) -> None:
         integral_batch_size=cfg.batch_size.integral_continuity,
         lambda_weighting={"normal_dot_vel": 0.1},
         criteria=Eq(x, channel_length[1]),
+        loss=modulus.sym.loss.IntegralLossNorm(name="integral_continuity"),
     )
     domain.add_constraint(integral_continuity, "integral_continuity")
 
