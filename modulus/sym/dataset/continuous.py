3ec9a7886d3940a43463de213e2949092f79f97b
HydrogenSulfate <490868991@qq.com>
2024-04-22 08:10:24 +0000

    add loading suffix in ContinuousPointwiseIterableDataset

--- modulus/sym/dataset/continuous.py
+++ modulus/sym/dataset/continuous.py
@@ -118,9 +118,9 @@ class ContinuousPointwiseIterableDataset(IterableDataset):
                 import os
                 debug_flag = bool(int(os.getenv("debug", 0)))
                 if debug_flag:
-                    invar = np.load(f"contiguous_pointwise_data/invar_torch_{self.iter_step}")
-                    outvar = np.load(f"contiguous_pointwise_data/outvar_torch_{self.iter_step}")
-                    lambda_weighting = np.load(f"contiguous_pointwise_data/lambda_weighting_torch_{self.iter_step}")
+                    invar = np.load(f"contiguous_pointwise_data/invar_torch_{self.iter_step}.npz")
+                    outvar = np.load(f"contiguous_pointwise_data/outvar_torch_{self.iter_step}.npz")
+                    lambda_weighting = np.load(f"contiguous_pointwise_data/lambda_weighting_torch_{self.iter_step}.npz")
                     invar = Dataset._to_tensor_dict(invar)
                     outvar = Dataset._to_tensor_dict(outvar)
                     lambda_weighting = Dataset._to_tensor_dict(lambda_weighting)
