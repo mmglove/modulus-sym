25769107349d7a3fb0ce269cb258e6889f8f3147
HydrogenSulfate <490868991@qq.com>
2024-04-23 05:01:17 +0000

    trigger python prim option when FLAGS_prim_all=True

--- modulus/sym/hydra/utils.py
+++ modulus/sym/hydra/utils.py
@@ -72,14 +72,13 @@ def main(config_path: str, config_name: str = "config"):
             # os.environ['PYTHONHASHSEED'] = str(GLOBAL_RANDOM_SEED)
 
             # Enable prim mode
-            # use_prim = bool(int(os.getenv("prim", True)))
-            # if use_prim:
-            #     print(f"✨ ✨ Prim = True, prim can be disabled by set 'prim=0 python example.py'")
-            #     paddle.framework.core.set_prim_eager_enabled(True)
-            #     paddle.framework.core._set_prim_all_enabled(True)
-            # else:
-            #     print(f"✨ ✨ Prim = False, prim can be disabled by set 'prim=0 python example.py'")
-            print(f"✨ ✨ FLAGS_prim_all = {os.getenv('FLAGS_prim_all', 'False') == 'True'}")
+            use_prim = os.getenv("FLAGS_prim_all", "False") == "True"
+            if use_prim:
+                print(f"✨ ✨ Prim = True, prim can be disabled by set 'prim=0 python example.py'")
+                paddle.framework.core.set_prim_eager_enabled(True)
+                paddle.framework.core._set_prim_all_enabled(True)
+            else:
+                print(f"✨ ✨ Prim = False, prim can be disabled by set 'prim=0 python example.py'")
 
             # Register all modulus groups before calling hydra main
             register_hydra_configs()
