6c9c36ec4161a5060e42070eef66422e887157c6
mmglove <gmm_email@163.com>
2024-04-28 15:53:18 +0800

    fix darcy data

--- test_tipc/dynamic/darcy-darcy_AFNO/benchmark_common/prepare.sh
+++ test_tipc/dynamic/darcy-darcy_AFNO/benchmark_common/prepare.sh
@@ -8,3 +8,10 @@ if [ ! -d './examples_sym' ]; then
     unzip examples_sym.zip
     \cp -r -f -v ./examples_sym/examples/* ./examples/
 fi
+
+if [ ! -d './examples/darcy/datasets' ]; then
+    mkdir -p ./examples/darcy/datasets && cd ./examples/darcy/datasets
+    wget wget https://paddle-qa.bj.bcebos.com/benchmark/pretrained/Darcy_241.tar.gz
+    tar xf Darcy_241.tar.gz
+    cd -
+fi
