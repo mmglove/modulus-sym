51643aabf218383adfa1c0f3cfc021152ec59f1a
mmglove <gmm_email@163.com>
2024-06-24 17:27:11 +0800

    fix paddle benchmark

--- test_tipc/dynamic/taylor_green-taylor_green_causal/benchmark_common/run_benchmark.sh
+++ test_tipc/dynamic/taylor_green-taylor_green_causal/benchmark_common/run_benchmark.sh
@@ -48,7 +48,7 @@ function _train(){
 #   以下为通用执行命令，无特殊可不用修改
 
     export DDE_BACKEND=paddle
-    train_cmd="pushd examples/taylor_green; python taylor_green_causal.py training.max_steps=600; popd"
+    train_cmd="pushd examples/taylor_green; python taylor_green_causal.py training.max_steps=2000; popd"
     echo "pwd: $PWD train_cmd: ${train_cmd} log_file: ${train_log_file}"
     set -x
     timeout 15m bash -c "${train_cmd}" > ${train_log_file} 2>&1
